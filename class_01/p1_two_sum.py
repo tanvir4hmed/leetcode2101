57aae71effb58a214dac491bca76a9b3f52c75e3
tanvir <tanvir@ishraak.com>
2021-07-23 23:01:18 +0600

    p1 two sum done

--- class_01/p1_two_sum.py
+++ class_01/p1_two_sum.py
@@ -6,3 +6,28 @@
 #         :rtype: List[int]
 #         """
 
+a = [2, 7, 11, 15]
+target = 9
+start = 0
+end = len(a) - 1
+templ_1 = 0
+templ_2 = 0
+temp = 0
+while start <= end:
+    if a[start] < target:
+        templ_1 = start
+        temp = target - a[templ_1]
+        templ_2 = templ_1 + 1
+        while templ_2 <= end:
+            if a[templ_2] == temp:
+                temp = 0
+                break
+            templ_2 += 1
+    if temp != 0:
+        start += 1
+    else:
+        break
+if start < end and a[templ_1] + a[templ_2] == target:
+    print(str(templ_1) + " and " + str(templ_2))
+else:
+    print("Not found")
