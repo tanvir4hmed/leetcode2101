1d9aed1241bba6bef0202e070381dbb7aaa4f5c7
tanvir <tanvir@ishraak.com>
2021-07-23 23:31:53 +0600

    two sum done

--- class_01/p1_two_sum.py
+++ class_01/p1_two_sum.py
@@ -1,33 +1,41 @@
-# class Solution(object):
-#     def twoSum(self, nums, target):
-#         """
-#         :type nums: List[int]
-#         :type target: int
-#         :rtype: List[int]
-#         """
-
-a = [2, 7, 11, 15]
-target = 9
-start = 0
-end = len(a) - 1
-templ_1 = 0
-templ_2 = 0
-temp = 0
-while start <= end:
-    if a[start] < target:
-        templ_1 = start
-        temp = target - a[templ_1]
-        templ_2 = templ_1 + 1
-        while templ_2 <= end:
-            if a[templ_2] == temp:
-                temp = 0
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        a = nums
+        target = target
+        start = 0
+        end = len(a) - 1
+        templist = [0, 0]
+        temp = 0
+        while start <= end:
+            if a[start] < target:
+                templist[0] = start
+                temp = target - a[templist[0]]
+                templist[1] = start + 1
+                while templist[1] <= end:
+                    if a[templist[1]] == temp:
+                        temp = 0
+                        break
+                    templist[1] += 1
+            if temp != 0:
+                start += 1
+            else:
                 break
-            templ_2 += 1
-    if temp != 0:
-        start += 1
+        if start < end and a[templist[0]] + a[templist[1]] == target:
+            return templist
+        else:
+            return 0
+
+
+if __name__ == '__main__':
+    a = [2, 7, 11, 15]
+    target = 8
+    result = Solution.twoSum(self=Solution, nums=a, target=target)
+    if result != 0:
+        print(result)
     else:
-        break
-if start < end and a[templ_1] + a[templ_2] == target:
-    print(str(templ_1) + " and " + str(templ_2))
-else:
-    print("Not found")
+        print("Not Found")
