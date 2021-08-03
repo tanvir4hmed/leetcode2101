c06aa6e93c52f9d071501837329b17389d8f2501
tanvir <tanvir@ishraak.com>
2021-08-03 23:55:27 +0600

    two sum done

--- class_01/p1_two_sum.py
+++ class_01/p1_two_sum.py
@@ -1,38 +1,8 @@
-# Time Complexity O(n^2) and Space Complexity O(1)
-
 class Solution(object):
     def twoSum(self, nums, target):
-        a = nums
-        target = target
-        start = 0
-        end = len(a) - 1
-        templist = [0, 0]
-        temp = 0
-        while start <= end:
-            if a[start] < target:
-                templist[0] = start
-                temp = target - a[templist[0]]
-                templist[1] = start + 1
-                while templist[1] <= end:
-                    if a[templist[1]] == temp:
-                        temp = 0
-                        break
-                    templist[1] += 1
-            if temp != 0:
-                start += 1
-            else:
-                break
-        if start < end and a[templist[0]] + a[templist[1]] == target:
-            return templist
-        else:
-            return 0
-
-
-if __name__ == '__main__':
-    a = [2, 7, 11, 15]
-    target = 8
-    result = Solution.twoSum(self=Solution, nums=a, target=target)
-    if result != 0:
-        print(result)
-    else:
-        print("Not Found")
+        dic = {}
+        for i,num in enumerate(nums):
+            if target - num in dic:
+                return dic[target-num],i
+            dic[num] = i
+        return
